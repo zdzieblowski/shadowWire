8469906ee6a6a78701631a68ee259f42211afbb2
Piotr Zdzieblowski <piotr@zdzieblowski.com>
2023-04-04 16:25:18 +0000

    invalid keys regeneration

--- _prototype/setup.py
+++ _prototype/setup.py
@@ -4,18 +4,16 @@ import os
 
 from Crypto.PublicKey import ECC
 
-###
+### CONFIG
 
 user_home = os.path.expanduser("~") + "/"
-
-#print(user_home)
 
 keychain_dir = "keychain/"
 prvkey_file = "sw_private.pem"
 pubkey_file = "sw_public.pem"
 contacts_file = "sw_contacts.json"
 
-###
+### INIT
 
 argp = argparse.ArgumentParser(formatter_class = argparse.ArgumentDefaultsHelpFormatter)
 argp.add_argument("-ds", "--data-storage", help = "define path to data storage", default = ".sw/")
@@ -28,8 +26,28 @@ keychain_storage = data_storage + keychain_dir
 prvkey_pem = keychain_storage + prvkey_file
 pubkey_pem = keychain_storage + pubkey_file
 
-#print(prvkey_pem)
-#print(pubkey_pem)
+### METHODS
+
+def del_keys():
+  os.remove(prvkey_pem)
+  os.remove(pubkey_pem)
+
+def gen_keys():
+  key = ECC.generate(curve="Ed448")
+
+  f = open(prvkey_pem,"xt")
+  f.write(key.export_key(format="PEM"))
+  f.close()
+
+  f = open(pubkey_pem,"xt")
+  f.write(key.public_key().export_key(format="PEM"))
+  f.close()
+
+  print("keys generated")
+
+  print(key)
+
+### RUNTIME
 
 if not os.path.isdir(data_storage):
   os.mkdir(data_storage)
@@ -44,19 +62,15 @@ if os.path.isfile(prvkey_pem) and os.path.isfile(pubkey_pem):
     print(import_key)
   except:
     print("invalid keys")
+    gen = input("generate new keys ? [y/N] ")
+    if gen in ("y","Y","n","N",""):
+      if gen in ("y","Y"):
+        del_keys()
+        gen_keys()
+      else:
+        print("exiting")
+    else:
+      print("wrong input")
 else:
   print("keys missing")
-
-  key = ECC.generate(curve="Ed448")
-
-  f = open(prvkey_pem,"xt")
-  f.write(key.export_key(format="PEM"))
-  f.close()
-
-  f = open(pubkey_pem,"xt")
-  f.write(key.public_key().export_key(format="PEM"))
-  f.close()
-
-  print("keys generated")
-
-  print(key)
+  gen_keys()
