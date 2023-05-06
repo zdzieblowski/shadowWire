01df44a6ae56d66714d10142f012b44c51e07eb8
Piotr Zdzieblowski <piotr@zdzieblowski.com>
2023-05-06 03:43:17 +0200

    travel continues

--- _prototype/server/src/server.js
+++ _prototype/server/src/server.js
@@ -1,3 +1,7 @@
+"use strict";
+
+const filesystem = require("fs");
+
 const server = require("express")();
 const parser = require("body-parser");
 const server_port = 60606;
@@ -8,11 +12,29 @@ server.use(parser.json());
 server.use(parser.urlencoded({ extended: false }));
 
 server.post("/read", (req, rep) => {
-    rep.json({reply: req.body})
+  console.log("request: download at height: " + req.body.height);
+
+  let data = JSON.parse(filesystem.readFileSync("fakedata.json"));
+  let selected_data = {};
+  let iter = 0;
+  for(let item in data.messages){
+    let obj_item = data.messages[item];
+    if(obj_item.height >= parseInt(req.body.height)){
+      selected_data[iter] = obj_item;
+      iter++;
+    }
+  }
+  rep.json(selected_data);
 })
 
 server.post("/write", (req, rep) => {
-    rep.json({reply: req.body})
+  let data = JSON.parse(filesystem.readFileSync("fakedata.json"));
+  data.messages.push({"height":data.messages.length, "data":req.body.message});
+
+  let modified_data = JSON.stringify(data);
+  filesystem.writeFileSync("fakedata.json", modified_data);
+
+  rep.json({reply: req.body.message})
 })
 
 //
