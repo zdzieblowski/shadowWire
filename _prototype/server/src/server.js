54528fb211967d4c197935a91f56fd7201d12f86
Piotr Zdzieblowski <piotr@zdzieblowski.com>
2023-05-11 01:23:53 +0200

    lowdb replaces static json file as server db

--- _prototype/server/src/server.js
+++ _prototype/server/src/server.js
@@ -1,60 +1,54 @@
 "use strict";
 
-const fs = require("fs");
-const os = require("os");
+import express from "express";
+import asyncHandler from "express-async-handler";
 
-const server = require("express")();
-const parser = require("body-parser");
+import { default as fs } from "fs";
+import { default as os } from "os";
+
+import { default as bp } from "body-parser";
+import { Low } from "lowdb";
+import { JSONFile } from 'lowdb-node';
+
+const server = express();
+server.use(express.json());
+
+const parser = bp;
+
 const server_port = 60606;
 
 //
 
-server.use(parser.json());
-server.use(parser.urlencoded({ extended: false }));
+let messages_dir = os.homedir() + "/.sws/";
+let messages_path = messages_dir + "messages.json";
 
-server.post("/read", (req, rep) => {
-  let messages_path = os.homedir() + "/.sws/messages.json";
+//
 
-  if (fs.existsSync(messages_path)) {
-    let data = JSON.parse(fs.readFileSync(messages_path));
-    let selected_data = [];
-    let iter = 0;
-    for (let item in data.messages) {
-      let obj_item = data.messages[item];
-      if (obj_item.height >= parseInt(req.body.height)) {
-        selected_data[iter] = obj_item;
-        iter++;
-      }
-    }
-    rep.json(selected_data);
-  } else {
-    rep.json({"error": "no messages available"});
-  }
-})
+const db = new Low(new JSONFile(messages_path), {});
 
-server.post("/write", (req, rep) => {
+if (!fs.existsSync(messages_dir)){
+  fs.mkdirSync(messages_dir);
+}
+if (!fs.existsSync(messages_path)) {
+  db.data = { messages: [] };
+  await db.write();
+}else{
+  await db.read();
+}
+//
 
-  let messages_dir = os.homedir() + "/.sws/";
-  let messages_path = messages_dir + "messages.json";
+server.post("/read", asyncHandler(async (req, res) => {
+  await db.read();
+  res.json(db.data.messages);
+}),
+)
 
-  if (fs.existsSync(messages_path)) {
-    let data = JSON.parse(fs.readFileSync(messages_path));
-    data.messages.push({"height":data.messages.length, "data": req.body.message});
-
-    let modified_data = JSON.stringify(data);
-    fs.writeFileSync(messages_path, modified_data);
-
-  } else {
-    if (!fs.existsSync(messages_dir)) {
-      fs.mkdirSync(messages_dir);
-    }
-    let modified_data = JSON.stringify({"messages": [{"height": 0, "data":req.body.message}]});
-    fs.writeFileSync(messages_path, modified_data);
-  }
-
-  rep.json({reply: req.body.message})
-
-})
+server.post("/write", asyncHandler(async (req, res) => {
+  db.data.messages.push({"data": req.body.message});
+  await db.write();
+  res.json("written");
+}),
+)
 
 //
 
