9fad95d5d83226937929da3eaa3f7c747bb3b202
Piotr Zdzieblowski <piotr@zdzieblowski.com>
2023-05-12 18:50:02 +0200

    testing utf-8

--- _prototype/_tests/bytecounter.py
+++ _prototype/_tests/bytecounter.py
@@ -1,4 +1,4 @@
-bytes = ["ążęźćńół", "abcdefgh", "aą", "ąa", "aa", "ąą"]
+bytes = ["ążęźćńół", "abcdefgh", "aą", "ąa", "aa", "ąą","塾の手紙","の","紙","補習班","習","班"]
 
 for b in bytes:
     print("%s %s" % (b, len(b.encode("utf-8"))))
